c32c0a18c71578629344975bafecac9343cf3bdd
Parq254 <juniorchris761@gmail.com>
2024-07-22 01:38:19 +0300

    javascript

---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log('Hello world')
+
+let num = 5
+var my_str = 'Chris'
+console.log(num)
+console.log(my_str)
+
+function printMyInput(user_input){
+    console.log('The parameter passed is '+ user_input)
+}
